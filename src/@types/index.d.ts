22580a1c5d098b45cb80ec5042156bc1c0bd33a9
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-01-10 20:55:42 -0300

    nothing

---
+++ src/@types/index.d.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+interface PropsWithChildren<T extends Record<any, any> = {}> {
+  children: React.ReactNode;
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export * from './types';
+
+export { Provider, Context } from './Context';
+
+export { default as Steps } from './components/Steps';
+export { default as Wizard } from './components/Wizard';
+
+export { default as useWizard } from './hooks/useWizard';
