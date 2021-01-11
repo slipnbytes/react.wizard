cb492c0dfee7e507a2fd362d951919828605e376
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-01-11 00:40:25 -0300

    fixes
    Co-authored-by: Gabriel Hijazi <heilhijzi@outlook.com>

--- src/components/Steps.tsx
+++ src/components/Steps.tsx
@@ -2,15 +2,20 @@ import React, { useEffect } from 'react';
 
 import useWizard from '../hooks/useWizard';
 import { StepsProps } from '../types';
+import mergeClassNames from '../utils/mergeClassNames';
 
-const Steps = ({ children }: StepsProps): JSX.Element => {
+const Steps = ({ children, className, ...props }: StepsProps): JSX.Element => {
   const { current, getStep, registerSteps } = useWizard();
 
   useEffect(() => {
     registerSteps(children);
   }, []);
 
-  return <div className="wizard-steps">{getStep(current)}</div>;
+  return (
+    <div {...props} className={mergeClassNames('wizard-steps', className)}>
+      {getStep(current)}
+    </div>
+  );
 };
 
 export default Steps;
--- src/components/Wizard.tsx
+++ src/components/Wizard.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 
 import { Provider } from '../Context';
 import { WizardProps } from '../types';
+import mergeClassNames from '../utils/mergeClassNames';
 
-const Wizard = ({ children }: WizardProps): JSX.Element => (
-  <Provider>{children}</Provider>
+const Wizard = ({
+  children,
+  className,
+  ...props
+}: WizardProps): JSX.Element => (
+  <Provider>
+    <div {...props} className={mergeClassNames('wizard', className)}>
+      {children}
+    </div>
+  </Provider>
 );
 
 export default Wizard;
