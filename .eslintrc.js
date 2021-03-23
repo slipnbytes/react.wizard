c5061cade723ea1dbc5d2fefa071449c7c8adcd3
Pablo Vinícius <dev.pablo.vinicius@gmail.com>
2021-03-23 15:59:42 -0300

    chore(eslint): usage @hitechline config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,6 @@
 module.exports = {
-  extends: '@hitechline/eslint-config-web',
-  rules: {
-    'no-shadow': 'off',
-    'no-unused-vars': 'off',
-    'no-use-before-define': 'off',
-
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        newlinesBetween: 'always',
-        alphabetize: { order: 'asc', ignoreCase: true },
-        groups: [
-          ['module', '/^@hitechline/'],
-          '/^@/',
-          ['parent', 'sibling', 'index'],
-        ],
-      },
-    ],
-
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/no-use-before-define': [
-      'error',
-      {
-        variables: false,
-        functions: false,
-        ignoreTypeReferences: true,
-      },
-    ],
-  },
+  extends: [
+    '@hitechline/eslint-config/web',
+    '@hitechline/eslint-config/typescript',
+  ],
 };
